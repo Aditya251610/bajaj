63d25660598b3b816de06820561635e9b5b1c559
Aditya251610 <aditya.sharma16062016@gmail.com>
2024-09-22 19:23:54 +0530

    deploy commit 10

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,28 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
+
 
 // Apply CORS before any routes, specifying allowed origins
-app.use(cors({
-  origin: ['https://bajaj-black-ten.vercel.app'] // Replace with your allowed origins
-}));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', '*'); // Or specify your allowed origin
+  res.header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, ' +
+    'Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
+app.use(express.json());
 
 app.use(bodyParser.json());
 
